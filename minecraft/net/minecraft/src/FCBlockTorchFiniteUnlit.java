7ad8e4491e7a357974e22b4ad54133a1c2b41733
Spicy-Boy <Doom.coproductions@gmail.com>
2023-10-18 23:41:10 -0400

    Added 'pop' sound to pickup
    Sound added to torches and bricks when right click picked up

--- minecraft/net/minecraft/src/FCBlockTorchFiniteUnlit.java
+++ minecraft/net/minecraft/src/FCBlockTorchFiniteUnlit.java
@@ -24,7 +24,8 @@ public class FCBlockTorchFiniteUnlit extends FCBlockTorchBaseUnlit
 //			System.out.println("EMPTY HAND DETECT!");
 
         		player.inventory.mainInventory[player.inventory.currentItem] = new ItemStack( FCBetterThanWolves.fcBlockTorchFiniteUnlit, 1);
-        		
+                player.worldObj.playSoundAtEntity( player, "random.pop", 0.2F, 
+                		((player.rand.nextFloat() - player.rand.nextFloat()) * 0.7F + 1.0F) * 2.0F);
         		world.setBlock(i, j, k, 0);
         		
         		return true;
